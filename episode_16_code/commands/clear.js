5ff5dfa20882e1f8c8b601dcee3b671d2b173611
XxAlooisxX <40740670+XxAlooisxX@users.noreply.github.com>
2018-07-26 12:41:21 +0300

    Update clear.js

--- episode_16_code/commands/clear.js
+++ episode_16_code/commands/clear.js
@@ -1,15 +1,23 @@
 const Discord = require("discord.js");
+client.on("message", message => {
+    var prefix = "-";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل ",
+        footer: {
+          text: "©KBOOSHDev"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
 
-module.exports.run = async (bot, message, args) => {
-
-  //!clear 15
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("oof.");
-  if(!args[0]) return message.channel.send("oof");
-  message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
-  });
-}
-
-module.exports.help = {
-  name: "clear"
-}
+     
+});
