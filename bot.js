8b1c119acb6fc30f60ee9938ea9e8674198039e5
XxAlooisxX <40740670+XxAlooisxX@users.noreply.github.com>
2018-07-27 11:41:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -260,7 +260,7 @@ client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : i1Suhaib`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+      console.log(`ON ${client.guilds.size} استمتع بالسيرفر ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(` *help - New Bot`,)
